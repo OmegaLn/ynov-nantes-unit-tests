6524378144383791d89c92f5ad33db95320b3792
OmegaLn <mohamedlaanadi1999@gmail.com>
2022-04-26 04:36:08 +0200

    Intro Workshop e2e-tests

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -1,5 +1,25 @@
+//const { browserContext } = this.helpers.Playwright;
+//const assert = require('assert')
+
 Feature('Ynov Nantes');
 
-Scenario('Test Ynov Nantes Land Page', ({ I }) => {
+Scenario('Test Ynov Nantes Land Page/Feature:Search Module', async ({ I }) => {
     I.amOnPage('https://www.ynov-nantes.com/');
+    I.click('.search-cta a.cta');
+    I.see('Que recherchez-vous ?','div');
+    I.fillField('input.searchfield', 'info');
+    
+    if (await I.grabTextFrom('.results-list .search-preview-cursus .preview-title .tile-title') == "Bachelor Informatique") {
+        I.say('Bachelor informatique found proceeding...');
+    }
+    
+    /*
+    l'implémentation des deux méthodes m'a permis de comparer les temps d'execution quant à la comparaison avec l'assert "Bachelor Informatique"
+    Avec un if classique le temps d'éxecution était de 7576 ms
+    Alors qu'avec la seconde méthode (utilisation du assert.equal pour comparer) le temps d'éxecution était de 7628ms 
+    Pour tester veuillez décommenter le bloc suivant :
+    
+    let search = await I.grabTextFrom('.results-list .search-preview-cursus .preview-title .tile-title');
+    assert.equal(search, 'Bachelor Informatique');
+    */
 });
