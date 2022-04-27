326cc4fe4de24479daf7287b5a6004cf92fc487c
OmegaLn <mohamedlaanadi1999@gmail.com>
2022-04-27 02:46:42 +0200

    Intro Workshop unit-tests

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -5,7 +5,98 @@ use PHPUnit\Framework\TestCase;
 require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
-    public function test_something() {
+    public function test_assert() {
         $this->assertEquals(42, 42);
     }
+    public function test_temp_isvalid() {
+        $temp_tracker = new TempTracker();
+        $this->expectException(ValueError::class);
+        $temp_tracker->insert(-1);
+    }
+    public function test_temp_type() {
+        $temp_tracker = new TempTracker();
+        $this->expectException(TypeError::class);
+        $temp_tracker->insert('str');
+    }
+    public function test_array_elements_type() {
+        $temp_tracker = new TempTracker();
+        $this->expectException(TypeError::class);
+        $temp_tracker->insert([1, 'str', 3]);
+    }
+    public function insert_temps() {
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(1);
+        $temp_tracker->insert(2);
+        $temp_tracker->insert(3);
+        $temp_tracker->insert(4);
+        $temp_tracker->insert(5);
+        $temp_tracker->insert(6);
+        $temp_tracker->insert(7);
+        $temp_tracker->insert(8);
+        $temp_tracker->insert(9);
+        $temp_tracker->insert(10);
+        $this->assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], $temp_tracker->get_temps());
+        $this->assertEquals(10, $temp_tracker->get_max());
+        $this->assertEquals(1, $temp_tracker->get_min());
+        $this->assertEquals(5.5, $temp_tracker->get_mean());
+    }
+    // les tests suivants ont été écrits pour tester les cas d'échec de tests comme les conditions dans /src/TempTracker.php ne seraient pas respectées
+    // pour tester ces conditions, vous pouvez décommenter les lignes suivantes et lancer la commande de test
+
+    /*
+    public function test_insert_temps_with_empty_array() {
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(1);
+        $temp_tracker->insert(2);
+        $temp_tracker->insert(3);
+        $temp_tracker->insert(4);
+        $temp_tracker->insert(5);
+        $temp_tracker->insert(6);
+        $temp_tracker->insert(7);
+        $temp_tracker->insert(8);
+        $temp_tracker->insert(9);
+        $temp_tracker->insert(10);
+        $temp_tracker->insert([]);
+        $this->assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, []], $temp_tracker->get_temps());
+        $this->assertEquals(10, $temp_tracker->get_max());
+        $this->assertEquals(1, $temp_tracker->get_min());
+        $this->assertEquals(5.5, $temp_tracker->get_mean());
+    }
+    public function test_insert_temps_with_strings() {
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(1);
+        $temp_tracker->insert(2);
+        $temp_tracker->insert(3);
+        $temp_tracker->insert(4);
+        $temp_tracker->insert(5);
+        $temp_tracker->insert(6);
+        $temp_tracker->insert(7);
+        $temp_tracker->insert(8);
+        $temp_tracker->insert(9);
+        $temp_tracker->insert(10);
+        $temp_tracker->insert('str');
+        $this->assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'str'], $temp_tracker->get_temps());
+        $this->assertEquals(10, $temp_tracker->get_max());
+        $this->assertEquals(1, $temp_tracker->get_min());
+        $this->assertEquals(5.5, $temp_tracker->get_mean());
+    }
+    public function test_insert_temps_with_strings_as_array_element() {
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(1);
+        $temp_tracker->insert(2);
+        $temp_tracker->insert(3);
+        $temp_tracker->insert(4);
+        $temp_tracker->insert(5);
+        $temp_tracker->insert(6);
+        $temp_tracker->insert(7);
+        $temp_tracker->insert(8);
+        $temp_tracker->insert(9);
+        $temp_tracker->insert(10);
+        $temp_tracker->insert(['str']);
+        $this->assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ['str']], $temp_tracker->get_temps());
+        $this->assertEquals(10, $temp_tracker->get_max());
+        $this->assertEquals(1, $temp_tracker->get_min());
+        $this->assertEquals(5.5, $temp_tracker->get_mean());
+    }
+    */
 }
--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -6,8 +6,51 @@ require_once('./src/flatten.php');
 
 class FlattenTest extends TestCase
 {
-    public function test_something()
-    {
-        $this->assertEquals(1337, 1337);
-    }
+    public function test_flatten_error_null()
+{
+    $this->expectException(TypeError::class);
+    $array = null;
+    flatten($array);
+}
+    public function test_flattening_strings()
+{
+    $this->expectException(TypeError::class);
+    $array = 'String';
+    flatten($array);
+}
+    public function test_flatten_isempty()
+{
+    $array = [];
+    $this->assertEquals([], flatten($array));
+    echo 'the array is empty';
+}
+    public function test_assert_equal()
+{
+    $this->assertEquals(1, 1);
+    $array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    $this->assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], flatten($array));
+}
+public function test_flattening_nested()
+{
+    $array = [1, 2, 4, 5, 6, [7, 8, 9], 10, [11, 12, [13, 14, [15, 16]]]];
+    $this->assertEquals([1, 2,  4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], flatten($array));
+}
+public function test_flattening_nested_and_empty()
+{
+    $array = [1, 2, 3, 4, 5, 6, [7, 8, 9], 10, [11, 12, [13, 14, [15, 16]]], []];
+    $this->assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], flatten($array));
+}
+public function test_flattening_nested_and_empty_and_strings()
+{
+    $array = [1, 2, 3, 4, 5, 6, [7, 8, 9], 10, [11, 12, [13, 14, [15, 16]]], [], 'string'];
+    $this->assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 'string'], flatten($array));
+
+}
+public function test_flattening_nested_and_empty_and_strings_and_null()
+{
+    $array = [1, 2, 3, 4, 5, 6, [7, 8, 9], 10, [11, 12, [13, 14, [15, 16]]], [], 'string', null];
+    $this->assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 'string', null], flatten($array));
+
+}
+
 }
